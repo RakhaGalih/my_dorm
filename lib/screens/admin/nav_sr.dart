3db0c071a2e5d4f7a8b0fe39adf22e7914788438
rakha <rakhagalih19@gmail.com>
2025-05-27 15:51:11 +0700

    memperbaiki UI lagi

--- lib/screens/admin/nav_sr.dart
+++ lib/screens/admin/nav_sr.dart
@@ -2,18 +2,17 @@ import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:my_dorm/components/main_nav_bar_home.dart';
 import 'package:my_dorm/models/navbar_model.dart';
-import 'package:my_dorm/screens/admin/home/home_page_helpdesk.dart';
 import 'package:my_dorm/screens/admin/home/home_page_sr.dart';
+import 'package:my_dorm/screens/admin/home/list_kamar_page_admin.dart';
 import 'package:my_dorm/screens/admin/home/notification_page_admin.dart';
 import 'package:my_dorm/screens/admin/home/profil_page_admin.dart';
-import 'package:my_dorm/screens/dormitizen/home/paket_page_dormitizen.dart';
 
 class NavbarSR extends StatelessWidget {
   final List<NavBarModel> navIcons = [
     NavBarModel(icon: FluentIcons.home_24_filled, title: 'Beranda'),
     NavBarModel(
-      icon: FluentIcons.box_24_filled,
-      title: 'Paket',
+      icon: FluentIcons.bed_24_filled,
+      title: 'kamar',
     ),
     NavBarModel(
       icon: FluentIcons.alert_24_filled,
@@ -26,7 +25,7 @@ class NavbarSR extends StatelessWidget {
   ];
   final List<Widget> widgetOptions = <Widget>[
     const HomePageSR(),
-    const PaketPageDormitizen(),
+    const ListKamarPageAdmin(),
     const NotificationPageAdmin(),
     const ProfilPageAdmin(),
   ];
