d169e8689c92c769f3cdd15a739aab8544e7f2f5
IksanRisandy <iksanrisandy@gmail.com>
2025-05-30 19:41:39 +0700

    integrasi API edit dan delete informasi

--- lib/components/info_card.dart
+++ lib/components/info_card.dart
@@ -7,13 +7,11 @@ import 'package:my_dorm/service/image_service.dart';
 
 class InformasiCard extends StatefulWidget {
   final Map<String, dynamic> item;
-  final VoidCallback? onEdit;
-  final VoidCallback? onDelete;
+  final VoidCallback? onHold;
   const InformasiCard({
     super.key,
     required this.item,
-    this.onEdit,
-    this.onDelete,
+    this.onHold,
   });
 
   @override
@@ -24,75 +22,82 @@ class _InformasiCardState extends State<InformasiCard> {
   int _maxLines = 5;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: kWhite,
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      elevation: 2,
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+    return GestureDetector(
+        onLongPress: widget.onHold,
+        child: Card(
+          color: kWhite,
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          elevation: 2,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const CircleAvatar(
-                  backgroundImage:
-                      AssetImage('images/zee.png'), // ganti path jika perlu
-                  radius: 20,
-                ),
-                const SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
                   children: [
-                    Text(widget.item['nama_penulis'],
-                        style: const TextStyle(fontWeight: FontWeight.bold)),
-                    Text(getFormattedDate(widget.item['updated_at']) ?? 'error',
-                        style:
-                            const TextStyle(color: Colors.grey, fontSize: 12)),
+                    const CircleAvatar(
+                      backgroundImage:
+                          AssetImage('images/zee.png'), // ganti path jika perlu
+                      radius: 20,
+                    ),
+                    const SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(widget.item['nama_penulis'],
+                            style:
+                                const TextStyle(fontWeight: FontWeight.bold)),
+                        Text(
+                            getFormattedDate(widget.item['updated_at']) ??
+                                'error',
+                            style: const TextStyle(
+                                color: Colors.grey, fontSize: 12)),
+                      ],
+                    ),
                   ],
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  widget.item['judul'] ?? 'error',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                Text(
+                  widget.item['isi'] ?? 'error',
+                  maxLines: _maxLines,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                if (widget.item['isi'].toString().length > 100)
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _maxLines = _maxLines == 5 ? 100 : 5;
+                      });
+                    },
+                    child: Text(
+                      (_maxLines == 5) ? 'Baca Selengkapnya' : 'Tutup',
+                      style: const TextStyle(
+                          color: Colors.red, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                const SizedBox(height: 10),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: MyNetworkImage(
+                    imageURL:
+                        '$apiURL/images/informasi/${widget.item['gambar']}',
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ],
             ),
-            const SizedBox(height: 10),
-            Text(
-              widget.item['judul'] ?? 'error',
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            const SizedBox(height: 6),
-            Text(
-              widget.item['isi'] ?? 'error',
-              maxLines: _maxLines,
-              overflow: TextOverflow.ellipsis,
-            ),
-            if (widget.item['isi'].toString().length > 100)
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _maxLines = _maxLines == 5 ? 100 : 5;
-                  });
-                },
-                child: Text(
-                  (_maxLines == 5) ? 'Baca Selengkapnya' : 'Tutup',
-                  style: const TextStyle(
-                      color: Colors.red, fontWeight: FontWeight.bold),
-                ),
-              ),
-            const SizedBox(height: 10),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: MyNetworkImage(
-                imageURL: '$apiURL/images/informasi/${widget.item['gambar']}',
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
